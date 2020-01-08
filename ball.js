889f55bfcf85ce0b90c7dc59e26fbc429393e154
mithu-wd <56589470+mithu-wd@users.noreply.github.com>
2020-01-09 02:39:10 +0530

    Update ball.js

--- ball.js
+++ ball.js
@@ -4,10 +4,10 @@ function Ball(){
   this.gravity=0.5;
   this.yspeed=0;
   this.force=-10
-  this.w=h;
+  this.w=this.h;
   this.show =function(){
     stroke(0);
-    ellipse(height/2/3,this.y,h,this.w);
+    ellipse(height/2/3,this.y,this.h,this.w);
     fill(255,0,0);
   }
   
@@ -20,14 +20,14 @@ function Ball(){
 ///      ()
   this.yspeed+=this.gravity;
   this.y+=this.yspeed;
-  if(this.y>height-h/2){
-  this.y=height-h/2; 
+  if(this.y>height-this.h/2){
+  this.y=height-this.h/2; 
   this.yspeed=0;
-  this.w=5 ;
+  this.w=5 
   }
   else
-  this.w=h;
-  if(this.y<h/2){
+  this.w=this.h;
+  if(this.y<height/2){
       
 this.y=height;  }
 
