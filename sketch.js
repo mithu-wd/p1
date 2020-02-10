00ef6274cbe9159115cd7ff9b99a09a4607f9063
mithu-wd <56589470+mithu-wd@users.noreply.github.com>
2020-02-10 12:04:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,44 +1,56 @@
 var ball;
-var need=[];
+var need = [];
 var vi;
+var button;
 var ni;
-var x=10;
+var x = 10;
+var score = 0;
 function setup() {
+  if (window.innerHeight > window.innerWidth) {
+    alert("Please use Landscape!");
+  }
+
   createCanvas(window.innerWidth, window.innerHeight);
-  ball= new Ball();
+  ball = new Ball();
   need.push(new Needles());
 }
 
-
 function draw() {
-  
   background(10);
-  for (var i=need.length-1; i>=0;i--){
-  need[i].sho();
-  need[i].update();
-  if (need[i].end()){
-    need.splice(i,1);
+
+  fill(255, 0, 0);
+  textSize(32);
+  text(parseInt(score), window.innerWidth - 110, 50);
+  for (var i = need.length - 1; i >= 0; i--) {
+    need[i].sho();
+    need[i].update();
+    if (need[i].end()) {
+      need.splice(i, 1);
+    }
+
+    if (need[i].hit(ball)) {
+      button = createButton("Restart");
+      button.position(window.innerWidth / 2, window.innerHeight / 2);
+      button.mousePressed(reload);
+      noLoop();
+    }
   }
- 
-  if(need[i].hit(ball)){
-noLoop();  }
- 
-}
   ball.update();
   ball.show();
-  
-  if (frameCount %40 ==0){
+  score += 0.5;
+
+  if (frameCount % 40 == 0) {
     need.push(new Needles());
-
   }
-
-
 }
-function keyPressed(){
-  if(key == ' '){
-  ball.up();}
+function keyPressed() {
+  if (key == " ") {
+    ball.up();
+  }
 }
-function mousePressed(){
+function mousePressed() {
   ball.up();
-
+}
+function reload() {
+  document.location.reload();
 }
